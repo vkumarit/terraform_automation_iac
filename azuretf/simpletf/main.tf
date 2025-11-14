a6777883f5ea00cd5342386cbe4a62a41dd530b0
signinvipin <signinvipin@gmail.com>
2025-11-14 12:57:24 +0530

    Add changes to provider block

--- azuretf/simpletf/main.tf
+++ azuretf/simpletf/main.tf
@@ -17,13 +17,16 @@ terraform {
 /* This is only required when the User, Service Principal, 
    or Identity running Terraform lacks the permissions to 
    register Azure Resource Providers.
-
-provider "azurerm" {
-  resource_provider_registrations = "none" 
-  features {}
-}
 */
-
+provider "azurerm" {
+#  resource_provider_registrations = "none" 
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
 
 # Resource group
 resource "azurerm_resource_group" "ample" {
@@ -71,6 +74,37 @@ resource "azurerm_subnet" "pvtnet" {
 }
 
 # Key Vault
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "amplekv" {
+  name                        = "amplekeyvault"
+  location                    = azurerm_resource_group.ample.location
+  resource_group_name         = azurerm_resource_group.ample.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
 
 # Storage account
 
