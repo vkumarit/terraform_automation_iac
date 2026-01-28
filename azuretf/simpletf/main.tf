b81715556e81628c6c1eeb19315dde0630ce3cfd
signinvipin <signinvipin@gmail.com>
2026-01-28 20:00:20 +0530

    Added resource for encryption

--- azuretf/simpletf/main.tf
+++ azuretf/simpletf/main.tf
@@ -158,7 +158,7 @@ resource "azurerm_key_vault" "prodmyapp" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   
   soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  purge_protection_enabled    = true       # for Encryption of storage accounts
   
   enable_rbac_authorization = true         # enabled RBAC (not using access polices)
   
