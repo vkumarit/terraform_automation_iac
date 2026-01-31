bf84a92352895e3f2a1385fb96ff9c21eacf2c01
signinvipin <signinvipin@gmail.com>
2026-01-31 12:12:03 +0530

    Added role assignment key vault Crypto Officer

--- azuretf/simpletf/main.tf
+++ azuretf/simpletf/main.tf
@@ -257,6 +257,13 @@ resource "azurerm_role_assignment" "storage_kv_crypto" {
   principal_id         = azurerm_user_assigned_identity.prodmyapp_sa_identity.principal_id
 }
 
+# Helps with permissions for viewing the keys via dashboard
+resource "azurerm_role_assignment" "human_kv_crypto_officer" {
+  scope                = azurerm_key_vault.prodmyapp.id
+  role_definition_name = "Key Vault Crypto Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 #Create Storage Account (CMK): Link the identity and key.
 resource "azurerm_storage_account" "prodmyapp_cmk" {
   name                     = "prodmyappsacmk01"
