e86e667a5d079fb8eae3493b8898df397f686610
signinvipin <signinvipin@gmail.com>
2026-03-05 19:06:39 +0530

    Corrected code0

--- azuretf/simpletf/main.tf
+++ azuretf/simpletf/main.tf
@@ -164,8 +164,8 @@ terraform {
     #key = "${var.environment}/terraform.tfstate"    
     #when keeping separate statefile for each environment
     
-    #use_azuread_auth     = true                      # When want to use entra id for authentication
-    #use_cli              = true  
+    use_azuread_auth     = false                      # When want to use entra id for authentication
+    use_cli              = false  
     # use_cli uses logged-in az cli context for authentication, comment out when switching to pipeline
   }
 }
