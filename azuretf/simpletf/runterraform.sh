bdb6f8f513d9666b6d25809c9a8370f8bec12940
signinvipin <signinvipin@gmail.com>
2026-03-28 23:54:07 +0530

    Added code for public IP output and reduced parallelism

--- azuretf/simpletf/runterraform.sh
+++ azuretf/simpletf/runterraform.sh
@@ -119,7 +119,7 @@ elif [[ "$COMMAND" == "plan" ]]; then
   else
     echo "Backend reachable. Running terraform plan..."
   
-    terraform plan -input=false -parallelism=5 -no-color -detailed-exitcode -lock-timeout=10m -out=tfplan.binary 2>&1 | tee "$LOG_FILE"
+    terraform plan -input=false -parallelism=2 -no-color -detailed-exitcode -lock-timeout=10m -out=tfplan.binary 2>&1 | tee "$LOG_FILE"
     # -parallelism=5 (only with plan & apply) > reduce memory usage 
     # -detailed-exitcode:
     #   0 > no changes
@@ -189,7 +189,7 @@ elif [[ "$COMMAND" == "apply" ]]; then
   # ------------------------------------------
   # Run terraform apply 
   # ------------------------------------------
-  terraform apply -input=false -parallelism=5 -no-color -auto-approve -lock-timeout=10m tfplan.binary 2>&1 | tee "$LOG_FILE"
+  terraform apply -input=false -parallelism=2 -no-color -auto-approve -lock-timeout=10m tfplan.binary 2>&1 | tee "$LOG_FILE"
   # -input=false 
   # -parallelism=5 (only with plan & apply) > reduce memory usage
   # -auto-approve skips manual confirmation
@@ -226,7 +226,7 @@ elif [[ "$COMMAND" == "apply" ]]; then
     echo "Detecting partially created/broken resources..."
     
     PLAN_OUT="tfplan.recovery"
-    terraform plan -input=false -parallelism=5 -lock-timeout=10m -no-color -out="$PLAN_OUT" -detailed-exitcode || true
+    terraform plan -input=false -parallelism=2 -lock-timeout=10m -no-color -out="$PLAN_OUT" -detailed-exitcode || true
     terraform show -json "$PLAN_OUT" > tfplan.recovery.json
 
     BROKEN_RESOURCES=$(jq -r '
@@ -248,7 +248,7 @@ elif [[ "$COMMAND" == "apply" ]]; then
 
         for res in $BROKEN_RESOURCES; do
             echo "Destroying $res in Azure..."
-            terraform destroy -target="$res" -auto-approve -parallelism=5 -lock-timeout=10m -no-color || true
+            terraform destroy -target="$res" -auto-approve -parallelism=2 -lock-timeout=10m -no-color || true
 
             echo "Removing $res from Terraform state..."
             terraform state rm "$res" || true
@@ -273,7 +273,7 @@ elif [[ "$COMMAND" == "apply" ]]; then
     Resources
     | where resourceGroup == 'myTFResourceGroup'
     | where tags.managed_by == 'terraform'
-    | where tags.creation_run_id == '$RUN_ID'
+    | where tags.terraform_run == '$RUN_ID'
     | project id, type
     " --query "data" -o json 2>/dev/null || echo "[]")
     # " | where resourceGroup == 'myTFResourceGroup' " - scoped orphaned resources scan search to RG,
