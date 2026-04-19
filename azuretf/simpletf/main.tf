1ca6ababaadb12181782099ae778a1b9cad91068
signinvipin <signinvipin@gmail.com>
2026-04-19 18:58:50 +0000

    Code format

--- azuretf/simpletf/main.tf
+++ azuretf/simpletf/main.tf
@@ -392,7 +392,7 @@ resource "azurerm_role_assignment" "storage_kv_crypto" {
 resource "azurerm_role_assignment" "human_kv_crypto_officer" {
   scope                = azurerm_key_vault.prodmyapp.id
   role_definition_name = "Key Vault Crypto Officer"
-  principal_id         = "USER_OBJECT_ID_HERE"        
+  principal_id         = "USER_OBJECT_ID_HERE"
   # Hardcode object IDs of known humans or service accounts. 
   # Like DevOps lead or security engineer’s object IDs. 
 }
