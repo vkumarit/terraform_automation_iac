e6a7e456397084e13aa986ab6351cc1fd82d3907
signinvipin <signinvipin@gmail.com>
2026-04-26 13:18:30 +0530

    Add secrets to key vault#3

--- azuretf/simpletf/main.tf
+++ azuretf/simpletf/main.tf
@@ -565,17 +565,17 @@ variable "github_token" {
 
 resource "azurerm_key_vault_secret" "github_token" {
   name  = "githubtoken"
-  value = var.github_token # var when exported TF_VAR_github_token to EC2/VM env vars
+  #value = var.github_token # var when exported TF_VAR_github_token to EC2/VM env vars
 
-  #value = "placeholder"
+  value = "placeholder"
   #Used when not managing secret rotation using terraform, az cli used for secret rotation
 
   key_vault_id = azurerm_key_vault.prodmyapp.id
 
   lifecycle {
-    ignore_changes = [] # Allow rotation, available value will be taken
+    #ignore_changes = [] # Allow rotation, available value will be taken
 
-    #ignore_changes = [value]
+    ignore_changes = [value]
     #Never update the secret after first creation, freeze secret forever
     #Used when not managing secret rotation using terraform, az cli used for secret rotation
 
